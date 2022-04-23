42aafe5b2ac5141a60735f7e2fbcdcf38088c17e
Flora Lê <flora.mt.le@hotmail.com>
2022-04-23 18:34:46 +0200

    Initial commit. Create a complete React app as SPA! Part1 Ch3

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+import '../../style/App.css'
+
+function Home() {
+  return <h1 className="App">Homepage</h1>
+}
+
+export default Home
---
+++ src/pages/Survey/index.jsx
@@ -0,0 +1,34 @@
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+function Survey() {
+  const { questionNumber } = useParams()
+  // let params = useParams()
+  const questionNumberInt = parseInt(questionNumber)
+  const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
+  const nextQuestionNumber = questionNumberInt + 1
+
+  return (
+    <div>
+      <h1>Questionnaire 🧮</h1>
+      {questionNumber ? (
+        <h2>Question {questionNumber}</h2>
+      ) : (
+        <Link to="/survey/1">Begin questionnaire</Link>
+      )}
+
+      {questionNumberInt > 1 && (
+        <Link to={'/survey/' + prevQuestionNumber}>Previous</Link>
+      )}
+      {questionNumberInt >= 1 && questionNumberInt < 10 && (
+        <Link to={'/survey/' + nextQuestionNumber}>Next</Link>
+      )}
+      {questionNumber && questionNumberInt === 10 && (
+        <Link to={'/results'}>Results</Link>
+      )}
+
+      {/* <h2>Question {params.questionNumber}</h2> */}
+    </div>
+  )
+}
+export default Survey
